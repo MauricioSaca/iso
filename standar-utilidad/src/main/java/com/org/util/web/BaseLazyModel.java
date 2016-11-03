dd36b72d3b60127d68b8f3a919f6d63ca48c6d76
Juan Renderos <juanito.anto@hotmail.com>
2016-11-02 18:50:40 -0600

    avances
    Change-Id: Iaa1def81837873f98ad96ae8efcdfb00e4614d7d

--- standar-utilidad/src/main/java/com/org/util/web/BaseLazyModel.java
+++ standar-utilidad/src/main/java/com/org/util/web/BaseLazyModel.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortMeta;
+import org.primefaces.model.SortOrder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -24,7 +25,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BaseLazyModel <T extends BaseModelEntity<ID>, ID extends Serializable> extends LazyDataModel<T>{
+public class BaseLazyModel<T extends BaseModelEntity<ID>, ID extends Serializable> extends LazyDataModel<T>{
 	
 	/**
 	 * 
@@ -46,22 +47,85 @@ public class BaseLazyModel <T extends BaseModelEntity<ID>, ID extends Serializab
 	
 	@Override
 	public List<T> load(int first, int pageSize, List<SortMeta> multiSortMeta, Map<String, Object> filters) {
+		Page<T> page = finder(first, pageSize, multiSortMeta, filters);
+		setRowCount((int) page.getTotalElements());
+		return page.getContent();
 
+	}
+	 
+	@Override
+    public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+        List<SortMeta> multiSortMeta = new ArrayList<SortMeta>();
+        /*List<SortMeta> sortFirst = sortFirst();
+        if (sortFirst != null && !sortFirst.isEmpty()) {
+            multiSortMeta.addAll(sortFirst);
+        }*/
+        if (sortField != null && sortOrder != null) {
+            String[] arrsorts = sortField.split("\\,");
+            if (arrsorts != null && arrsorts.length > 0) {
+                for (String sort : arrsorts) {
+                    SortMeta sortMeta = new SortMeta();
+                    sortMeta.setSortField(sort);
+                    sortMeta.setSortOrder(sortOrder);
+                    multiSortMeta.add(sortMeta);
+                }
+            }
+        }
+        /*List<SortMeta> sortLast = sortLast();
+        if (sortLast != null && !sortLast.isEmpty()) {
+            multiSortMeta.addAll(sortLast);
+        }*/
+
+        Page<T> page = finder(first, pageSize, multiSortMeta, filters);
+        setRowCount((int) page.getTotalElements());
+        
+        return page.getContent();
+    }
+	
+	protected Page<T> finder(int first, int pageSize, List<SortMeta> multiSortMeta, Map<String, Object> filters) {
 		if (multiSortMeta != null) {
 			sortingRecord = multiSortMeta;
 		}
 		Pageable pageable = PaginatorHelper.pageable(first, pageSize, sortingRecord);
 		Set<ValueHolder> conditions = PaginatorHelper.transformer(generalSearchFields(), filters, entityClass);
-        if (conditions == null) {
-            conditions = new HashSet<ValueHolder>();
-        }
-        conditions.addAll(getCustomFilters());
-		Page<T> page = service.findAll(pageable, any, true, conditions, new HashSet<GroupedFilter>());
-		
-		this.setRowCount((int) page.getTotalElements());
-		
-		return page.getContent();
+		if (conditions == null) {
+			conditions = new HashSet<ValueHolder>();
+		}
+		conditions.addAll(getCustomFilters());
+		return service.findAll(pageable, isAny(), isCustomFilter(), conditions, new HashSet<>());
 	}
+
+	/**
+	 * este es el bueno
+	 * @return
+	 */
+    @Override
+    public T getRowData(String rowKey) {
+        T ret = null;
+        if (service != null) {
+            ID recid = null;
+            Class idclass = service.getIdClazz();
+            if (idclass.equals(String.class)) {
+                recid = (ID) rowKey;
+            } else if (idclass.equals(Long.class)) {
+                recid = (ID) new Long(rowKey);
+            } else if (idclass.equals(Integer.class)) {
+                recid = (ID) new Integer(rowKey);
+            }
+
+            ret = service.findOne(recid);
+        }
+        return ret;
+    }
+    
+    @Override
+    public ID getRowKey(T element) {
+        return element.getId();
+    }
+    
+    public boolean isCustomFilter() {
+        return customFilters != null && !customFilters.isEmpty();
+    }
 	
 	public Set<ValueHolder> getCustomFilters() {
         if (customFilters == null) {
@@ -100,29 +164,4 @@ public class BaseLazyModel <T extends BaseModelEntity<ID>, ID extends Serializab
     public void clearGroupedFilters() {
         getGroupedFilters().clear();
     }
-    
-	@Override
-    public T getRowData(String rowKey) {
-        T ret = null;
-        if (service != null) {
-            ID recid = null;
-            Class idclass = service.getClass();
-            if (idclass.equals(String.class)) {
-                recid = (ID) rowKey;
-            } else if (idclass.equals(Long.class)) {
-                recid = (ID) new Long(rowKey);
-            } else if (idclass.equals(Integer.class)) {
-                recid = (ID) new Integer(rowKey);
-            }
-
-            ret = service.findOne(recid);
-        }
-        return ret;
-    }
-
-    @Override
-    public ID getRowKey(T element) {
-        return element.getId();
-    }
-
 }
--- standar-web/src/main/java/com/org/web/pets/views/PetView.java
+++ standar-web/src/main/java/com/org/web/pets/views/PetView.java
@@ -9,6 +9,8 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.omnifaces.cdi.ViewScoped;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
 
 import com.org.adoption.model.Cat;
 import com.org.adoption.model.Dog;
@@ -48,9 +50,19 @@ public class PetView implements Serializable{
 	private List<PetGender> petGenderList;
 	
 	/**
+	 * TRUE for create FALSE for edit
+	 */
+	private boolean createOrEdit;
+	
+	/**
 	 * True for cats, False for Dogs
 	 */
 	private boolean catDogFlag;
+	
+	/**
+	 * TRUE for add/edit, FALSE for list view
+	 */
+	private boolean renderEditView = false;
 	
 	@PostConstruct
 	public void init(){
@@ -82,19 +94,102 @@ public class PetView implements Serializable{
 		return catLazyData;
 	}
 
-	public void registerPet() {
-		if (catDogFlag) {
-			catService.save(selectedCat);
-		} else {
-			dogService.save(selectedDog);
-		}
+	public void showEditPanel(){
+		renderEditView = true;
+		createOrEdit = true;
 	}
 	
-	public void updatePet(){
+	public Cat getCatFromDog(Dog dog){
+		Cat cat = new Cat();
+		cat.setId(dog.getId());
+		cat.setCatName(dog.getDogName());
+		cat.setBreed(dog.getBreed());
+		cat.setAge(dog.getAge());
+		cat.setWeight(dog.getWeight());
+		cat.setHeight(dog.getHeight());
+		cat.setBehavior(dog.getBehavior());
+		cat.setDescription(dog.getDescription());
+		cat.setPetOrigin(dog.getPetOrigin());
+		cat.setPetStatus(dog.getPetStatus());
+		cat.setPetGender(dog.getPetGender());
+		return cat;
+	}
+	
+	public Dog getDogFromCat(Cat cat){
+		Dog dog = new Dog();
 		
+		if(cat.getId() != null){
+			dog.setId(cat.getId());			
+		}
+		
+		dog.setDogName(cat.getCatName());
+		dog.setBreed(cat.getBreed());
+		dog.setAge(cat.getAge());
+		dog.setWeight(cat.getWeight());
+		dog.setHeight(cat.getHeight());
+		dog.setBehavior(cat.getBehavior());
+		dog.setDescription(cat.getDescription());
+		dog.setPetOrigin(cat.getPetOrigin());
+		dog.setPetStatus(cat.getPetStatus());
+		dog.setPetGender(cat.getPetGender());
+		return dog;
+	}
+	
+	public void registerPet() {
+		try {
+			
+			if (catDogFlag) {
+				catService.save(selectedCat);
+			} else {
+				selectedDog = getDogFromCat(selectedCat);			
+				dogService.save(selectedDog);
+			}
+			Messages.create("REGISTRO").detail("Registro agregado exitosamente").add();
+			renderEditView = false;
+		} catch (Exception e) {
+			selectedCat = new Cat();
+			selectedDog = new Dog();
+			Messages.create("EXCEPTION").detail("ERROR: " + e.getMessage()).error().add();
+		}
+		
+		selectedCat = new Cat();
+		selectedDog = new Dog();
+	}
+	
+	public void prepareUpdate(boolean isCat){
+		createOrEdit = false;
+		renderEditView = true;
+		selectedCat = !isCat ? getCatFromDog(selectedDog) : selectedCat;
+		catDogFlag = !isCat ? false : true;
+	}
+	
+	public void updatePet() {
+		try {
+			if (catDogFlag) {
+				catService.save(selectedCat);
+			} else {
+				selectedDog = getDogFromCat(selectedCat);
+				dogService.save(selectedDog);
+			}
+
+			Messages.create("REGISTRO").detail("Registro actualizado exitosamente").add();
+			renderEditView = false;
+		} catch (Exception e) {
+			selectedCat = new Cat();
+			selectedDog = new Dog();
+			Messages.create("EXCEPTION").detail("ERROR: " + e.getMessage()).error().add();
+		}
+		selectedCat = new Cat();
+		selectedDog = new Dog();
 	}
 
-	public void deletePet(){
-		
+	public void deleteCat(Cat cat){
+		catService.deleteOne(cat);
+		Messages.create("REGISTRO").detail("Eliminado exitosamente").add();
+	}
+	
+	public void deleteDog(Dog dog){
+		dogService.deleteOne(dog);
+		Messages.create("REGISTRO").detail("Eliminado exitosamente").add();
 	}
 }
